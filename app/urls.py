2a42c73fb81c49b39dafe4e9e8d0dbdfe73af9e6
oxunjon <oxunjoncoder>
2024-01-28 17:52:14 +0500

    template completed

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
-from app.views import IndexView
+from app.views import index_view, about_view, rooms_view, room_details_view, services_view, restaurant_view, blog_view, \
+    post_view, contact_view, bad_request_view
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', index_view, name='index'),
+    path('about/', about_view, name='about'),
+    path('rooms/', rooms_view, name='rooms'),
+    path('room_details/', room_details_view, name='room-details'),
+    path('services/', services_view, name='services'),
+    path('restaurant/', restaurant_view, name='restaurant'),
+    path('blog/', blog_view, name='blog'),
+    path('post/', post_view, name='post'),
+    path('contact/', contact_view, name='contact'),
+    path('404/', bad_request_view, name='404'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
 
 
-class IndexView(TemplateView):
-    template_name = 'app/index.html'
+def index_view(request):
+    return render(request, 'app/index.html')
+
+
+def about_view(request):
+    return render(request, 'app/about.html')
+
+
+def rooms_view(request):
+    return render(request, 'app/rooms.html')
+
+
+def room_details_view(request):
+    return render(request, 'app/room-details.html')
+
+
+def services_view(request):
+    return render(request, 'app/services.html')
+
+
+def restaurant_view(request):
+    return render(request, 'app/restaurant.html')
+
+
+def blog_view(request):
+    return render(request, 'app/blog.html')
+
+
+def post_view(request):
+    return render(request, 'app/post.html')
+
+
+def contact_view(request):
+    return render(request, 'app/contact.html')
+
+
+def bad_request_view(request):
+    return render(request, 'app/404.html')
